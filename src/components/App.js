02c8e49c5cf4f2608ded6d0e0272d375737eafbd
safeer997 <143935408+safeer997@users.noreply.github.com>
2024-12-01 10:18:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <p>Hello , Welcome to React ! </p>
+    <p><h1>I am learning React. My life is getting better.</h1> </p>
     </div>
     
   )
